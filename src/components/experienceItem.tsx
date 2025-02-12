24ac4df88fcbab3667c21ecc03c1cf6b9eaef315
Nick <nicolasrenanmachado@gmail.com>
2025-02-12 12:34:52 -0300

    Changed names from Portuguese to English

--- src/components/experienceItem.tsx
+++ src/components/experienceItem.tsx
@@ -3,27 +3,27 @@ import project from "../assets/project.svg";
 import clock from "../assets/clock.svg";
 import description from "../assets/description.svg";
 
-interface itemProps {
-  projeto: string,
-  tempo: number,
-  descricao: string,
+export interface experienceItemProps {
+  project: string,
+  months: number,
+  description: string,
   className?: string
 }
 
-export default function ExperienceItem(props: itemProps) {
+export default function ExperienceItem(props: experienceItemProps) {
   return (
     <li className={`flex flex-col gap-0.5 ${props.className}`}>
       <div className="flex items-center gap-1" >
         <img className="size-5 select-none" alt="Ícone indicando projeto" src={project}/>
-        <h3>{props.projeto}</h3>
+        <h3>{props.project}</h3>
       </div>
       <div className="flex items-center gap-1" >
         <img className="size-5 select-none" alt="Ícone indicando tempo" src={clock}/>
-        <h3>{props.tempo} meses</h3>
+        <h3>{props.months} meses</h3>
       </div>
       <div className="flex items-start gap-1" >
         <img className="size-5 select-none" alt="Ícone indicando descrição" src={description}/>
-        <p>{props.descricao}</p>
+        <p>{props.description}</p>
       </div>
     </li>
   )
