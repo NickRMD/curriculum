477b49269281b24a765023f23676be61650ce7a1
Nick <nicolasrenanmachado@gmail.com>
2025-02-12 12:33:48 -0300

    Changed content and header to components

---
+++ src/components/content.tsx
@@ -0,0 +1,39 @@
+import ExperienceItem from "../components/experienceItem";
+import experiences from "../resources/experiences";
+
+export default function Content() {
+
+  return (
+    <>
+      <main className="flex flex-col rounded-tr-md rounded-br-md h-[calc(100vh-1rem)] px-2.5 py-2 w-screen mr-2 mt-2 mb-2 bg-stone-300">
+        <div className="flex flex-col p-1 border-b border-black" id="resumo">
+          <h1 className="text-xl text-black rounded-md">Resumo</h1>
+          <p>
+            Programador com experiência em diversas linguagens de programação como por exemplo: Javascript, Typescript, Rust, Go (Golang),
+            PHP e C (neste último ainda aprendendo). Tive duas experiências profissionais completas até o momento, trabalhando com ao menos
+            algumas das linguagens citadas e em meu tempo livre utilizando-se de minhas habilidades em meus repositórios do&nbsp;
+            <a target="_blank" href="https://github.com/NickRMD">GitHub</a>.
+          </p>
+        </div>
+        <div className="p-1">
+          <h2 className="text-xl">Experiências profissionais</h2>
+          <ul className="flex gap-2 flex-col">
+            {
+              experiences.map(
+                (exp, i) => <
+                ExperienceItem 
+                  key={i}
+                  className={`text-base ${experiences[i+1] ? "border-b pb-1" : null}`}
+                  project={exp.project}
+                  description={exp.description}
+                  months={exp.months}
+                />
+              )
+            }
+          </ul>
+        </div>
+      </main>
+    </>
+  )
+
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,87 @@
+
+import avatar from "../assets/avatar.webp";
+import contact from "../assets/contact.svg";
+import language from "../assets/language.svg"
+import science from "../assets/science.svg";
+import lasso from "../assets/lasso.svg";
+import AbilityItem from "../components/abilityItem";
+import abilities from "../resources/abilities";
+import SocialNetworks from "../components/socialNetworks";
+import socialNetworkList from "../resources/socialNetworks";
+
+const languages = [
+  { name: "Português", level: "Nativo" },
+  { name: "Inglês", level: "Avançado" },
+];
+
+export default function Header() {
+
+  return (
+    <>
+      <header className={`
+        flex flex-col 
+        bg-emerald-950 rounded-tl-md 
+        rounded-bl-md ml-2 
+        mt-2 mb-2 
+        px-2.5 h-[calc(100vh-1rem)] 
+        w-[40rem] sm:w-[32rem] md:w-[34rem] lg:w-[28rem] xl:w-[26rem]
+        py-2 gap-2 
+        text-white`}>
+        <div className="flex text-center justify-center">
+          <img src={avatar} className="size-32 rounded-full"/>
+        </div>
+        <h1 className="text-4xl text-emerald-300 text-justify">
+          Nícolas Renan<div className="text-3xl  text-emerald-100">Machado Dias</div>
+        </h1>
+        <div className="flex flex-col gap-0.5">
+          <div className="flex items-center gap-1">
+            <img className="size-5 select-none white-svg" alt="Ícone para representar contatos" src={contact}/>
+            <h2 className="text-emerald-300">Contatos</h2>
+          </div>
+          <SocialNetworks socialNetworks={socialNetworkList}/>
+        </div>
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-1">
+            <img className="size-5 select-none white-svg" alt="Ícone para representar linguagens" src={language}/>
+            <h2 className="text-emerald-300">Linguagens</h2>
+          </div>
+          <ul className="pl-2 border-l border-white space-y-1">
+            {languages.map(({ name, level }, i) => (
+              <li 
+                key={i}
+                className={`
+                  grid grid-cols-[5rem_2rem_5rem] 
+                  text-center justify-around
+                  items-center gap-3 
+                  ${languages[i+1] ? "border-white pb-0.5" : "border-transparent"} border-b
+                `}>
+                <span>{name}</span>
+                <span className="text-center"><img className="size-7 select-none white-svg" alt="Laço" src={lasso}/></span>
+                <span>{level}</span>
+              </li>
+              ))}
+          </ul>
+        </div>
+        <div>
+          <div className="flex items-center gap-1">
+            <img className="size-5 select-none white-svg" alt="Ícone de linguagens" src={science}/>
+            <h2 className="text-emerald-300">Habilidades e competências</h2>
+          </div>
+          <ul className="flex flex-col gap-0.5 mt-0.5 pl-1 ml-0.5 border-l border-white">
+            {
+              abilities.map((ability, i) => (
+                <AbilityItem 
+                  key={i}
+                  src={ability.src} 
+                  alt={ability.alt} 
+                  listClasses={ability.listClasses} 
+                  imageClasses={ability.imageClasses}>{ability.children}</AbilityItem>
+              ))
+            }
+          </ul>
+        </div>
+      </header>
+    </>
+  )
+
+}
