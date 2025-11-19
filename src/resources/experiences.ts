32d89ccdde94a231be2cffed7dfb944ffe9eb39d
Nicolas Renan Machado Dias <nicolasrenanmachado@gmail.com>
2025-11-19 13:54:56 -0300

    fix/months

--- src/resources/experiences.ts
+++ src/resources/experiences.ts
@@ -5,7 +5,7 @@ type experienceList = experienceItemProps[];
 const experiences: experienceList = [
   // {
   //   project: "WYB - With Your Blessing",
-  //   months: 5,
+  //   months: 6,
   //   description: `
   //   Neste projeto fiz o uso de Next.js e Express.js usando PostgreSQL (database) para montar o site para o jogo específico em que o cliente (frontend)
   //   se comunicava com o servidor (backend) para buscar informações do jogador e etc. para mostrar no site e poder fazer pagamentos para
@@ -14,7 +14,7 @@ const experiences: experienceList = [
   // },
   {
     project: "Nexsi",
-    months: 12,
+    months: 14,
     description: `
     Neste outro projeto fiz o uso de Express.js, AWS (SQS e Lambda), Oracle Object Storage, Golang (linguagem de programação), React (usando Vite),
     PostgreSQL, CockroachDB e diversas outras tecnologias para codificação e documentação do sistema que conectava ERPs com a loja da Nexsi em diversos marketplaces, 
